742010d740b5628c071683d957137bbde5d7c23a
Syrimande <caterinadasilva@gmail.com>
2017-07-10 20:53:27 -0400

    Etapa 7 terminada: Se imprime mensaje en consola que muestra que el documento HTML se ha cargado, desde script-recipe.js

--- assets/js/script-recipe.js
+++ assets/js/script-recipe.js
@@ -1,5 +1,7 @@
 $(document).ready( function(){
 	// Función para mostrar y ocultar botones con íconos del menú.
 	$("#js-menu").remove();
-	
+
+	// Alert
+	console.log("El documento HTML se ha cargado.")
 });
